2e89ba3dfff7e873935c936c3cc4b48c4a3c9300
roeibenzion <92427776+roeibenzion@users.noreply.github.com>
2023-06-08 12:10:30 +0300

    Update dqn_learn.py

--- dqn/dqn_learn.py
+++ dqn/dqn_learn.py
@@ -194,7 +194,11 @@ def dqn_learing(
         #Store last observation
         idx = replay_buffer.store_frame(last_obs)
         #Encode recent observation
-        obs = replay_buffer.encode_recent_observation()
+         if t == 0:
+            obs = np.concatenate((last_obs, last_obs, last_obs, last_obs), 2)
+            obs = np.moveaxis(obs, -1, 0)
+         else:
+            obs = replay_buffer.encode_recent_observation()
         #Select action
         action = select_epilson_greedy_action(Q, obs, t)
         #Step environment
