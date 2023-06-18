16dbf3eb096af68d4d286e11e631056852f2b2a0
roeibenzion <92427776+roeibenzion@users.noreply.github.com>
2023-06-18 11:58:41 +0300

    Update dqn_learn.py

--- dqn/dqn_learn.py
+++ dqn/dqn_learn.py
@@ -226,7 +226,7 @@ def dqn_learing(
         obs = replay_buffer.encode_recent_observation()
         #Select action
         #action = select_epilson_greedy_action(Q, obs, t)
-        action = select_epilson_greedy_action(Q, obs, t, mean_episode_reward[max(0,t-1)], mean_episode_reward[max(0, t-2)])
+        action = select_epilson_greedy_action(Q, obs, t, Statistic['mean_episode_reward'][max(0,t-1)], Statistic['mean_episode_reward'][max(0,t-2)])
         #Step environment
         obs, reward, done, info = env.step(action)
         #Store effect
@@ -337,12 +337,12 @@ def dqn_learing(
                 pickle.dump(Statistic, f)
                 print("Saved to %s" % '/statistics.pkl')
             
-            '''
+            
             import shutil
 
             # Copy the file to Google Drive
             shutil.copy('statistics.pkl', '/content/drive/MyDrive/statistics.pkl')
-            '''
+            
             
             
             
