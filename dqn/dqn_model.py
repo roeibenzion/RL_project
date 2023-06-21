456dd72b184089692377402a48786a51d4027167
roeibenzion <92427776+roeibenzion@users.noreply.github.com>
2023-06-21 17:14:50 +0300

    Update dqn_model.py

--- dqn/dqn_model.py
+++ dqn/dqn_model.py
@@ -1,7 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-'''
 class DQN(nn.Module):
     def __init__(self, in_channels=4, num_actions=18):
         """
@@ -23,27 +22,6 @@ class DQN(nn.Module):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
-        x = F.relu(self.fc4(x.view(x.size(0), -1)))
-        return self.fc5(x)
-'''
-class DQN(nn.Module):
-    def __init__(self, in_channels=4, num_actions=18):
-        super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=8, stride=4)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        self.bn3 = nn.BatchNorm2d(64)
-        self.fc4 = nn.Linear(7 * 7 * 64, 512)
-        #self.bn4 = nn.BatchNorm1d(512)
-        self.fc5 = nn.Linear(512, num_actions)
-
-    def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        #x = F.relu(self.bn4(self.fc4(x.view(x.size(0), -1))))
         x = F.relu(self.fc4(x.view(x.size(0), -1)))
         return self.fc5(x)
 
